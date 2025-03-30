3009b71db4f465c83fa84356ed600cd860685267
KnightSoul9 <satysatyam14@gmail.com>
2025-03-30 23:20:46 +0530

    removed the sentry

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Blogs from "@/components/Blogs";
 
-const Home = () => {
+const Home = () => { 
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
